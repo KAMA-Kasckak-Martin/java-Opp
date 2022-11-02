aaeb8a7cbbf98a5396dcf433ec190fda2c647003
martinkascak <m.kascak93@gmail.com>
2022-11-02 17:26:11 +0100

    Add tests ohne ende

---
+++ src/test/java/org/example/StudentDBTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.example.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentDBTest {
+
+    @org.junit.jupiter.api.Test
+    void addStudent() {
+        //given
+        Student julia = new Student();
+        Student sergej = new Student();
+        List<Student> nennDieAnders = new ArrayList<>();
+        StudentDB studentsDb = new StudentDB(nennDieAnders);
+
+        //when
+        studentsDb.addStudent(julia);
+        studentsDb.addStudent(sergej);
+
+
+        //then
+        assertEquals(studentsDb.studentList.size(),2);
+
+    }
+}
